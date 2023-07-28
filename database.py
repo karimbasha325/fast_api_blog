a37ad0476afd152c1c8784eb1df9e25efc17b99e
karim <47456204+karimbasha325@users.noreply.github.com>
2023-07-28 17:04:06 +0530

    Update database.py

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://root:root@localhost:3306/fast_api"
+SQLALCHEMY_DATABASE_URL = "mysql://user:password@localhost:3306/{database_name}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
